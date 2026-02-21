a8a81470266ea0a587277efd22a2a1fd3c0fd787
Amir Alam <sameer72135@gmail.com>
2026-02-22 04:24:28 +0530

    Crreated home page.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,26 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import {useFonts} from 'expo-font';
+import { useEffect } from "react";   
 import '../global.css'
 
 
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+  "QuickSand-Bold": require('.././assets/font/Quicksand-Bold.ttf'),
+  "QuickSand-Medium": require('.././assets/font/Quicksand-Medium.ttf'),
+  "QuickSand-Regular": require('.././assets/font/Quicksand-Regular.ttf'),
+  "QuickSand-SemiBold": require('.././assets/font/Quicksand-SemiBold.ttf'),
+  "QuickSand-Light": require('.././assets/font/Quicksand-Light.ttf'),
+});
+
+useEffect(() => {
+  if (error) throw error;
+
+  if (fontsLoaded) {
+    SplashScreen.hideAsync();
+  }
+}, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{headerShown: false}}/>
 }
