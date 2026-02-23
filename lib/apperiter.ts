b10dd91ce247d542cafc260b2883013dd19bd5cb
Amir Alam <sameer72135@gmail.com>
2026-02-24 04:23:07 +0530

    connected with appwriter.

---
+++ lib/apperiter.ts
@@ -0,0 +1,6 @@
+
+export const appwriterConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    Platform: "food-delivery.com",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME
+}
